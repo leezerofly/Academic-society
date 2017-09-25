b930884fd9e9c3a7e857c976a7e2a9082d4e0e4c
leezerofly <leezerofly@gmail.com>
2017-09-25 13:43:57 +0800

    数据库更新

--- _installation/02-create-academic-society.sql
+++ _installation/02-create-academic-society.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 2017-09-23 13:15:37
+-- Generation Time: 2017-09-25 05:42:25
 -- 服务器版本： 5.7.14
 -- PHP Version: 7.0.10
 
@@ -31,9 +31,17 @@ CREATE TABLE `article` (
   `article_type_id` int(10) UNSIGNED DEFAULT NULL,
   `article_title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `article_content` text COLLATE utf8_unicode_ci NOT NULL,
-  `article_user_id` int(11) DEFAULT NULL,
+  `article_user_id` int(10) UNSIGNED DEFAULT NULL,
   `article_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- 转存表中的数据 `article`
+--
+
+INSERT INTO `article` (`article_id`, `article_type_id`, `article_title`, `article_content`, `article_user_id`, `article_time`) VALUES
+(11, 5, 'sdadas ', '<p>adsssssss<img src="http://img.baidu.com/hi/jx2/j_0034.gif"/></p>', 1, '2017-09-24 14:50:36'),
+(12, 6, 'dadasda ', '<p>sdasd asd as das asd&nbsp;</p>', 1, '2017-09-24 14:50:43');
 
 -- --------------------------------------------------------
 
@@ -54,7 +62,11 @@ INSERT INTO `article_type` (`type_id`, `type_name`) VALUES
 (1, '学会新闻'),
 (2, '通知公告'),
 (3, '文章类型3'),
-(4, '文章类型4');
+(4, '文章类型4'),
+(5, '文章类型5'),
+(6, '文章类型6'),
+(7, '文章类型7'),
+(8, '文章类型8');
 
 -- --------------------------------------------------------
 
@@ -63,11 +75,20 @@ INSERT INTO `article_type` (`type_id`, `type_name`) VALUES
 --
 
 CREATE TABLE `users` (
-  `user_id` int(11) NOT NULL COMMENT 'auto incrementing user_id of each user, unique index',
+  `user_id` int(10) UNSIGNED NOT NULL COMMENT 'auto incrementing user_id of each user, unique index',
   `user_name` varchar(64) COLLATE utf8_unicode_ci NOT NULL COMMENT 'user''s name, unique',
   `user_password_hash` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT 'user''s password in salted and hashed format',
-  `user_email` varchar(64) COLLATE utf8_unicode_ci NOT NULL COMMENT 'user''s email, unique'
+  `user_email` varchar(64) COLLATE utf8_unicode_ci NOT NULL COMMENT 'user''s email, unique',
+  `user_type` int(10) UNSIGNED DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='user data';
+
+--
+-- 转存表中的数据 `users`
+--
+
+INSERT INTO `users` (`user_id`, `user_name`, `user_password_hash`, `user_email`, `user_type`) VALUES
+(1, 'admin', '$2y$10$I.D86APbZ3QjzQSefAUVnOb3OFoHKJovDc34GRDbsnSTl0BWXNvCu', 'admin@qq.com', 1),
+(2, 'user1', '$2y$10$uNcIyqviqTMizw0bFJzGUeADhk4Ir878NbFS3NurY0377WZsKPahG', 'user1@qq.com', NULL);
 
 --
 -- Indexes for dumped tables
@@ -104,17 +125,17 @@ ALTER TABLE `users`
 -- 使用表AUTO_INCREMENT `article`
 --
 ALTER TABLE `article`
-  MODIFY `article_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `article_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 --
 -- 使用表AUTO_INCREMENT `article_type`
 --
 ALTER TABLE `article_type`
-  MODIFY `type_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `type_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 --
 -- 使用表AUTO_INCREMENT `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'auto incrementing user_id of each user, unique index';
+  MODIFY `user_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'auto incrementing user_id of each user, unique index', AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
