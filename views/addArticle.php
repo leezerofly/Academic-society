7bb90b5659be9af3b244a1dd688094415a9b6a90
leezerofly <leezerofly@gmail.com>
2017-11-12 12:29:28 +0800

    发表文章页面样式修改

--- views/addArticle.php
+++ views/addArticle.php
@@ -44,7 +44,7 @@
 </head>
 <body>
   <div class="mainpart">
-    <form class="form-horizontal" action="addArticle.php" method="post">
+    <form  action="addArticle.php" method="post">
         <!-- 文章标题 -->
         <div class="form-group">
             <label for="articleTitle">文章标题</label>
@@ -77,7 +77,7 @@
         <script id="editor" name="articleContent" type="text/plain" style="width:100%;height:500px;"></script>
 
         <!-- 发表按钮 -->
-        <input type="submit" class="btn btn-info" name="addBtn" value="发表"/>
+        <input type="submit" class="btn btn-info btn-publish center-block" name="addBtn" value="发表"/>
     </form>
   </div>
 
@@ -87,25 +87,29 @@
     //建议使用工厂方法getEditor创建和引用编辑器实例，如果在某个闭包下引用该编辑器，直接调用UE.getEditor('editor')就能拿到相关的实例
     // var ue = UE.getEditor('editor');
     var ue = UE.getEditor('editor',{  
-        //这里可以选择自己需要的工具按钮名称,此处仅选择如下五个  
+        //自定义工具栏
         toolbars:[[
-            'fullscreen', 
+            'fullscreen',
             'undo', 'redo', '|',
-            'bold', 'italic', 'underline', 'fontborder', 'strikethrough', 'superscript', 'subscript', 'removeformat', 'formatmatch', 'autotypeset', 'blockquote', 'pasteplain', '|', 'forecolor', 'backcolor', 'insertorderedlist', 'insertunorderedlist', 'selectall', 'cleardoc', '|',
-            'rowspacingtop', 'rowspacingbottom', 'lineheight', '|',
             'customstyle', 'paragraph', 'fontfamily', 'fontsize', '|',
-            'directionalityltr', 'directionalityrtl', 'indent', '|',
-            'justifyleft', 'justifycenter', 'justifyright', 'justifyjustify', '|', 'touppercase', 'tolowercase', '|',
-            'link', 'unlink', 'anchor', '|', 'imagenone', 'imageleft', 'imageright', 'imagecenter', '|',
-            'simpleupload', 'insertimage', 'emotion', 'scrawl', 'insertvideo', 'music', 'attachment', 'map', 'gmap', 'insertframe', 'insertcode', 'webapp', 'pagebreak', 'template', 'background', '|',
-            'horizontal', 'date', 'time', 'spechars', 'snapscreen', 'wordimage', '|',
+            'justifyleft', 'justifycenter', 'justifyright', 'justifyjustify', '|',
+            'bold', 'italic', 'underline', 'fontborder', 'strikethrough', 'superscript', 'subscript', 'autotypeset', 'blockquote', 'pasteplain', 
+        ],
+        [
+            'forecolor', 'backcolor', 'insertorderedlist', 'insertunorderedlist', 'selectall', 'cleardoc', '|',
+            'rowspacingtop', 'rowspacingbottom', 'lineheight', '|',
+            'imagenone', 'imageleft', 'imageright', 'imagecenter', '|',
+            'simpleupload', 'insertimage', 'emotion', 'scrawl', 'insertvideo', 'music', 'attachment', 'map', 'template', '|',
+            'horizontal', 'date', 'time', 'spechars', 'snapscreen',
+        ],
+        [
             'inserttable', 'deletetable', 'insertparagraphbeforetable', 'insertrow', 'deleterow', 'insertcol', 'deletecol', 'mergecells', 'mergeright', 'mergedown', 'splittocells', 'splittorows', 'splittocols', 'charts', '|',
-            'print', 'preview', 'searchreplace', 'drafts', 'help'
+            'help'
         ]],  
         //focus时自动清空初始化时的内容  
         autoClearinitialContent:true,  
-        //关闭字数统计  
-        wordCount:false,  
+        //字数统计  
+        wordCount:true,  
         //关闭elementPath  
         elementPathEnabled:false,  
         //默认的编辑区域高度  
